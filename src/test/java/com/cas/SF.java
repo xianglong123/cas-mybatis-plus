b748eeea14e181e032396d008445372eacad8add
xianglong <1391086179@qq.com>
2021-03-13 21:44:20 +0800

    配置druid

---
+++ src/test/java/com/cas/SF.java
@@ -0,0 +1,34 @@
+package com.cas;
+
+import org.junit.Test;
+
+/**
+ * @author: xianglong[1391086179@qq.com]
+ * @date: 下午9:12 2021/1/18
+ * @version: V1.0
+ * @review:
+ */
+public class SF {
+
+
+    /**
+     * bcabcdf
+     * abcabcde
+     * bcabcd
+     */
+    @Test
+    public void test() {
+
+    }//202101211611196672722
+
+
+    public String result(String a, String b) {
+        // 记录最长的长度
+        int count = 0;
+        char[] aChar = a.toCharArray();
+        char[] bChar = b.toCharArray();
+        return "";
+    }
+
+
+}
